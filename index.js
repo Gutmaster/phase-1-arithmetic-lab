4174f7e07b26388f68728389c90fa93ee5bfef55
+ <+>
2024-01-08 16:29:45 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+let num1 = 2
+let num2 = 31
+let num3 = 11
+let num4 = 7
+
+const multiply = num1*num2
+
+const random = Math.floor(Math.random())*10 + 1
+
+const mod = num3%num4
+
+const max = Math.max(0, 18, 13, 20, 5)
